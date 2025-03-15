9cd4eec647a00bb6a98340f17b87267a56930463
ccfelius <felius@cwi.nl>
2025-03-15 20:48:48 +0100

    removed comment

--- src/core/functions/scalar/encrypt_naive.cpp
+++ src/core/functions/scalar/encrypt_naive.cpp
@@ -209,8 +209,6 @@ void EncryptToEtype(LogicalType result_struct, Vector &input_vector,
   // global, local and encryption state
   auto &lstate = VCryptFunctionLocalState::ResetAndGet(state);
   auto vcrypt_state = GetVCryptState(state);
-
-  // Get Key from Bind
   auto key = VCryptBasicFun::GetKey(state);
 
   // Reset the reference of the result vector
